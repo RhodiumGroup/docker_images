ea4ddcaf50b0fa7f0df89eb4eaba68aefbb904c7
jgerardsimcock <justin_simcock@yahoo.com>
2018-03-09 17:41:46 -1000

    update with new config setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 # Start cluster on Google cloud
-gcloud container clusters create pangeo --num-nodes=10 --machine-type=n1-standard-2 --zone=us-central1-b  --cluster-version=1.9.3-gke.0
+gcloud container clusters create jhub-rhg --num-nodes=2 --machine-type=n1-standard-2 --zone=us-west1-a  --cluster-version=1.9.3-gke.0
 
-gcloud container clusters get-credentials pangeo --zone us-central1-b --project pangeo-181919
+#gcloud container clusters get-credentials pangeo --zone us-central1-b --project pangeo-181919
 
 # Set up Kubernetes
-kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=jhamman1@gmail.com
+kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=jsimcock@rhg.com
 kubectl --namespace kube-system create sa tiller
 kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
 helm init --service-account tiller
@@ -15,10 +15,10 @@ helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
 helm repo update
 
 # Install JupyterHub and Dask on the cluster
-helm install jupyterhub/jupyterhub --version=v0.6.0-9701a90 --name=jupyter --namespace=pangeo -f jupyter-config.yaml -f secret-config.yaml
+helm install jupyterhub/jupyterhub --version=v0.6.0-9701a90 --name=jupyter --namespace=jhub-rhg -f jupyter-config.yaml
 
 # Look for publised services.  Route domain name A records to these IPs.
-kubectl get services --namespace pangeo
+kubectl get services --namespace jhub-rhg
 # Look for dask-scheduler
 #      and proxy-...
 
