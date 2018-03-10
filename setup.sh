c9bc2d0c22b100ff551ce808e1151e5e2953f368
jgerardsimcock <justin_simcock@yahoo.com>
2018-03-09 17:54:56 -1000

    update k8s image name

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
 helm repo update
 
 # Install JupyterHub and Dask on the cluster
-helm install jupyterhub/jupyterhub --version=v0.6.0-9701a90 --name=jupyter --namespace=jhub-rhg -f jupyter-config.yaml
+helm install jupyterhub/jupyterhub --version=v0.6.0-9701a90 --name=jupyter --namespace=jhub-rhg -f jupyter-config.yml
 
 # Look for publised services.  Route domain name A records to these IPs.
 kubectl get services --namespace jhub-rhg
