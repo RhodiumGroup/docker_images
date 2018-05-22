1b3258380a38798cf7cf1047f1c801469c2241f3
jgerardsimcock <justin_simcock@yahoo.com>
2018-05-22 13:36:43 -1000

    change all references to worker-template

--- notebook/add_service_creds.py
+++ notebook/add_service_creds.py
@@ -3,7 +3,7 @@ import json, yaml
 
 def add_service_creds():
 
-    with open('/home/jovyan/worker-template.yaml', 'r') as f:
+    with open('/home/jovyan/worker-template.yml', 'r') as f:
         WORKER_TEMPLATE = yaml.safe_load(f)
 
     for env in WORKER_TEMPLATE['spec']['containers'][0]['env']:
@@ -18,7 +18,7 @@ def add_service_creds():
     WORKER_TEMPLATE['spec']['containers'][0]['env'].append(
         {'name': 'GCSFUSE_TOKEN', 'value': TOKEN_STRING})
 
-    with open('/home/jovyan/worker-template.yaml', 'w') as f:
+    with open('/home/jovyan/worker-template.yml', 'w') as f:
         f.write(yaml.dump(WORKER_TEMPLATE))
 
     print('worker-template.yml updated')
