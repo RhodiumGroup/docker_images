de802908038c7ec84aa276359b2b2f5bc2005e54
Michael Delgado <delgado.michaelt@gmail.com>
2018-09-06 22:05:18 -0700

    place config file in .config/dask/config.yaml

--- notebook/prepare.sh
+++ notebook/prepare.sh
@@ -2,13 +2,14 @@
 
 set -x
 
-echo "Copy files from pre-load directory into home"
-cp --update -r -v /pre-home/. /home/jovyan
-
 echo "Copy Dask configuration files from pre-load directory into home/.config"
 mkdir -p /home/jovyan/.config/dask
-cp --update -r -v /pre-home/config.yml /home/jovyan/.config/dask/
+cp --update -r -v /pre-home/config.yaml /home/jovyan/.config/dask/
 cp --update -r -v /pre-home/worker-template.yml /home/jovyan/.config/dask/
+sudo rm /pre-home/config.yaml
+
+echo "Copy files from pre-load directory into home"
+cp --update -r -v /pre-home/. /home/jovyan
 
 if [[ -e "/opt/app/environment.yml" ]]; then
     echo "environment.yml found. Installing packages";
