b9e29475f7bc682d266727d139942a54174420b5
Ian Bolliger <bolliger32@gmail.com>
2019-10-05 09:01:09 -0700

    user permission wrangling

--- common.sh
+++ common.sh
@@ -1,5 +1,5 @@
- #!/bin/sh
- apt-get update --fix-missing --no-install-recommends
+#!/bin/sh
+apt-get update --fix-missing --no-install-recommends
 
 apt-get install -yq --no-install-recommends apt-utils \
     wget bzip2 ca-certificates curl git gnupg2 apt-transport-https
@@ -7,9 +7,9 @@ apt-get install -yq --no-install-recommends apt-utils \
 # install google cloud sdk
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] \
   https://packages.cloud.google.com/apt cloud-sdk main" | \
-  sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+  tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
-  sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+  apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 apt-get update -y
 apt-get install google-cloud-sdk kubectl -y
 
