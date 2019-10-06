a3a8172d2bfbc25751f7bbde4910a90a0a7f4b44
Ian Bolliger <bolliger32@gmail.com>
2019-10-06 10:00:44 -0700

    new google cloud install

--- common.sh
+++ common.sh
@@ -4,34 +4,36 @@
 apt-get update --no-install-recommends -y
 apt-get install -yq --no-install-recommends \
   apt-utils \
-  wget \
   bzip2 \
   ca-certificates \
   curl \
-  gnupg2
+  lsb-release \
+  gnupg2 \
+  sudo
 
-# install gcsfuse
+# install gcsfuse, google cloud sdk, kubectl
 # (need curl to be installed earlier)
 export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
-echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | tee /etc/apt/sources.list.d/gcsfuse.list
+echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | \
+  tee /etc/apt/sources.list.d/gcsfuse.list
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] \
+  https://packages.cloud.google.com/apt cloud-sdk main" | \
+  tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-apt-get install -yq --no-install-recommends gcsfuse
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
+  apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+apt-get install -yq --no-install-recommends gcsfuse google-cloud-sdk kubectl
 alias googlefuse=/usr/bin/gcsfuse
 
 apt-get clean
 
-# install google cloud sdk
-# echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] \
-#   https://packages.cloud.google.com/apt cloud-sdk main" | \
-#   tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-# curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
-#   apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-# apt-get update -y
-# apt-get install google-cloud-sdk kubectl -y
-
 # get cloud sql proxy
 wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O /usr/bin/cloud_sql_proxy
 chmod +x /usr/bin/cloud_sql_proxy
+
+# conda updates
+conda update -n base conda
+conda config --set channel_priority strict
 
 # filepath curating
 chmod +x /usr/bin/prepare.sh
