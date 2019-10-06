4f1d2565cb8835ff5a754acf3916348c1698e6c9
Ian Bolliger <bolliger32@gmail.com>
2019-10-06 09:04:07 -0700

    try cloud-sdk from conda

--- common.sh
+++ common.sh
@@ -1,29 +1,28 @@
 #!/bin/sh
-apt-get update --fix-missing --no-install-recommends
 
-apt-get install -yq --no-install-recommends apt-utils \
-    wget bzip2 ca-certificates curl git gnupg2 apt-transport-https
-
-# install google cloud sdk
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] \
-  https://packages.cloud.google.com/apt cloud-sdk main" | \
-  tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
-  apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-apt-get update -y
-apt-get install google-cloud-sdk kubectl -y
-
-# install gcsFUSE
+# add gcsfuse dist. URL to apt as package source & import public key
 export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
 echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | tee /etc/apt/sources.list.d/gcsfuse.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-apt-get update -y
-apt-get install gcsfuse -y
+
+# install apt-get packages
+apt-get update --no-install-recommends -y
+apt-get install -yq --no-install-recommends apt-utils \
+    wget bzip2 ca-certificates curl git gnupg2 gcsfuse
+apt-get upgrade -yq --no-install-recommends
+apt-get clean
+
+# set up aliases
 alias googlefuse=/usr/bin/gcsfuse
 
-apt-get upgrade -yq --no-install-recommends
-
-apt-get clean
+# install google cloud sdk
+# echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] \
+#   https://packages.cloud.google.com/apt cloud-sdk main" | \
+#   tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+# curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
+#   apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+# apt-get update -y
+# apt-get install google-cloud-sdk kubectl -y
 
 # get cloud sql proxy
 wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O /usr/bin/cloud_sql_proxy
