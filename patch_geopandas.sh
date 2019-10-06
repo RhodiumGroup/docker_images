15d8cfa2523b71f6c105a00e927e6488bbf8ff9d
Ian Bolliger <bolliger32@gmail.com>
2019-10-06 16:29:21 -0700

    fix dumb geopandas patch typo

--- patch_geopandas.sh
+++ patch_geopandas.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sed -i "s/from pandas.core.internals import Block, NonConsolidatableMixIn, \
 BlockManager/from pandas.core.internals.blocks import Block, \
-NonCnsolidatableMixIn\nfrom pandas.core.internals.managers import \
+NonConslidatableMixIn\nfrom pandas.core.internals.managers import \
 BlockManager/g" /opt/conda/lib/python3.6/site-packages/geopandas/_block.py
