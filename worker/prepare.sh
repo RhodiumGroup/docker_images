a8296753f4614bd67335d72ac7fdaaca57cc9620
Ian Bolliger <bolliger32@gmail.com>
2019-10-23 08:00:18 -0700

    drop extraneous prepare.sh code

--- worker/prepare.sh
+++ worker/prepare.sh
@@ -2,13 +2,6 @@
 
 set -x
 
-if [[ -e "/opt/app/environment.yml" ]]; then
-    echo "environment.yml found. Installing packages"
-    /opt/conda/bin/conda env update -f /opt/app/environment.yml
-else
-    echo "no environment.yml"
-fi
-
 if [[ "$EXTRA_CONDA_PACKAGES" ]]; then
     echo "EXTRA_CONDA_PACKAGES environment variable found.  Installing."
     /opt/conda/bin/conda install --yes $EXTRA_CONDA_PACKAGES
