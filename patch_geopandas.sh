6d35766b6b86daedbbb55115682fcf6070ce1a81
Michael Delgado <delgadom@users.noreply.github.com>
2019-10-29 09:06:44 -0700

    document geopandas patch

--- patch_geopandas.sh
+++ patch_geopandas.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# manually edit geopandas/label/dev to match current pandas deployment
+# see https://github.com/geopandas/geopandas/issues/473#issuecomment-465060849
+# should be removed if geopandas core becomes vectorized, whether the
+# cythonized branch gets merged in or the geometry calculations are handled
+# by pyGEOS, either through shapely or replacing shapely. See
+# https://github.com/Toblerity/Shapely/issues/782.
 sed -i "s/from pandas.core.internals import Block, NonConsolidatableMixIn, \
 BlockManager/from pandas.core.internals.blocks import Block, \
 NonConsolidatableMixIn\nfrom pandas.core.internals.managers import \
