f4e6b30405d3f4cdaba67083e41b6c53b0639db0
Michael Delgado <delgado.michaelt@gmail.com>
2019-12-13 16:30:42 -0800

    oops. suuuuuper dumb bug.

--- common.sh
+++ common.sh
@@ -41,4 +41,4 @@ mkdir /opt/app
 # super sketchy hack to get around our need for compiler_compat binaries and some
 # other things that cause problems together?
 # see https://github.com/ContinuumIO/anaconda-issues/issues/11152
-RUN -rf /opt/conda/compiler_compat/ld
+rm -rf /opt/conda/compiler_compat/ld
