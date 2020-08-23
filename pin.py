b7050d68d2b875205d60db874e6361820a28d802
Ian Bolliger <ibolliger@rhg.com>
2020-08-23 11:15:41 -0700

    update pin to rm r_env

--- pin.py
+++ pin.py
@@ -28,8 +28,7 @@ from ruamel.yaml import YAML
 SPEC_FILES = [
             ('shared_resources/base_environment.yml', 'base'),
             ('notebook/notebook_environment.yml', 'base'),
-            ('octave-worker/octave_environment.yml', 'base'),
-            ('notebook/r_environment.yml', 'r')]
+            ('octave-worker/octave_environment.yml', 'base')]
 
 
 def get_versions_in_current_environment(envname='base'):
